5b107b1b62bc4c7f94e39aed39c47ca0c76637a3
Agustina Boujon <agus.boujon10@gmail.com>
2019-05-02 12:53:40 -0300

    Initial commit

---
+++ src/main/java/server/servercloud/ServerCloudApplication.java
@@ -0,0 +1,13 @@
+package server.servercloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerCloudApplication.class, args);
+	}
+
+}
